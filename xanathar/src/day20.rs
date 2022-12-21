2e30e3c2611699d706ce3573152fde9488152bfc
Marco Mastropaolo <marco@mastropaolo.com>
2022-12-21 09:50:52 +0100

    [xanathar] Day 20 & 21

--- xanathar/src/day20.rs
+++ xanathar/src/day20.rs
@@ -1,11 +1,101 @@
-#![allow(unused_imports)]
 use crate::utils;
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque};
 use std::iter::Iterator;
-use std::ops::RangeInclusive;
 
-pub fn test1() {}
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Hash)]
+struct Number {
+    val: i64,
+    order: usize,
+}
 
-pub fn test2() {}
+impl Number {
+    pub fn new(val: i64, order: usize) -> Self {
+        Self { val, order }
+    }
+}
+
+fn rearrange_with_swaps_mod(data: &mut Vec<Number>, index: usize) {
+    let bias = data[index].val.abs() / ((data.len() as i64) - 1);
+    let bias = if data[index].val < 0 { -1 - bias } else { bias };
+
+    let val = (data[index].val + bias).rem_euclid(data.len() as i64);
+
+    let sign = val.signum();
+    let mut swaps = 0;
+    let mut index = index;
+
+    while swaps != val {
+        let new_index =
+            (((index as isize) + (sign as isize) + (data.len() as isize)) as usize) % data.len();
+
+        data.swap(index, new_index);
+        index = new_index;
+        swaps += sign;
+    }
+}
+
+pub fn test1() {
+    let raw_data = utils::parse_lines("./data/day20.txt", |s| s.parse::<i64>().ok());
+    let mut data = raw_data
+        .iter()
+        .enumerate()
+        .map(|(i, v)| Number::new(*v, i))
+        .collect::<Vec<_>>();
+
+    for i in 0..data.len() {
+        let index_to_swap = data
+            .iter()
+            .enumerate()
+            .find(|(_, item)| item.order == i)
+            .map(|(idx, _)| idx)
+            .unwrap();
+        rearrange_with_swaps_mod(&mut data, index_to_swap);
+    }
+
+    let zero_idx = data
+        .iter()
+        .enumerate()
+        .find(|(_, item)| item.val == 0)
+        .map(|(idx, _)| idx)
+        .unwrap();
+
+    let n1 = data[(1000 + zero_idx) % data.len()].val;
+    let n2 = data[(2000 + zero_idx) % data.len()].val;
+    let n3 = data[(3000 + zero_idx) % data.len()].val;
+
+    println!("{}+{}+{} = {}", n1, n2, n3, n1 + n2 + n3);
+}
+
+pub fn test2() {
+    let raw_data = utils::parse_lines("./data/day20.txt", |s| s.parse::<i64>().ok());
+    let mut data = raw_data
+        .iter()
+        .map(|v| v * 811589153)
+        .enumerate()
+        .map(|(i, v)| Number::new(v, i))
+        .collect::<Vec<_>>();
+
+    for _ in 0..10 {
+        for i in 0..data.len() {
+            let index_to_swap = data
+                .iter()
+                .enumerate()
+                .find(|(_, item)| item.order == i)
+                .map(|(idx, _)| idx)
+                .unwrap();
+            rearrange_with_swaps_mod(&mut data, index_to_swap);
+        }
+    }
+
+    let zero_idx = data
+        .iter()
+        .enumerate()
+        .find(|(_, item)| item.val == 0)
+        .map(|(idx, _)| idx)
+        .unwrap();
+
+    let n1 = data[(1000 + zero_idx) % data.len()].val;
+    let n2 = data[(2000 + zero_idx) % data.len()].val;
+    let n3 = data[(3000 + zero_idx) % data.len()].val;
+
+    println!("{}+{}+{} = {}", n1, n2, n3, n1 + n2 + n3);
+}
--- xanathar/src/day21.rs
+++ xanathar/src/day21.rs
@@ -1,11 +1,151 @@
-#![allow(unused_imports)]
 use crate::utils;
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 use std::iter::Iterator;
-use std::ops::RangeInclusive;
 
-pub fn test1() {}
+enum OpType {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
 
-pub fn test2() {}
+enum Yell {
+    Num(i64),
+    Op(OpType, String, String),
+}
+
+struct Monkey {
+    yell: Yell,
+    name: String,
+}
+
+impl Monkey {
+    pub fn parse(line: String) -> Option<Self> {
+        let mut chars = VecDeque::from_iter(line.chars());
+        let name = Self::parse_name(&mut chars);
+        Self::discard_char(&mut chars, ':');
+        Self::discard_char(&mut chars, ' ');
+
+        let yell = match chars.front() {
+            None => panic!("Unexpected end of string"),
+            Some(c) if c.is_ascii_digit() => Yell::Num(Self::parse_number(&mut chars)),
+            Some(_) => {
+                let operand1 = Self::parse_name(&mut chars);
+                Self::discard_char(&mut chars, ' ');
+                let op_type = match chars.pop_front() {
+                    None => panic!("Operand expected"),
+                    Some('+') => OpType::Add,
+                    Some('-') => OpType::Sub,
+                    Some('*') => OpType::Mul,
+                    Some('/') => OpType::Div,
+                    Some(c) => panic!("Unexpected operand '{}'", c),
+                };
+                Self::discard_char(&mut chars, ' ');
+                let operand2 = Self::parse_name(&mut chars);
+                Yell::Op(op_type, operand1, operand2)
+            }
+        };
+
+        Some(Self { name, yell })
+    }
+
+    fn parse_name(chars: &mut VecDeque<char>) -> String {
+        let mut s = String::with_capacity(4);
+        s.push(chars.pop_front().expect("Character expected"));
+        s.push(chars.pop_front().expect("Character expected"));
+        s.push(chars.pop_front().expect("Character expected"));
+        s.push(chars.pop_front().expect("Character expected"));
+        s
+    }
+
+    fn discard_char(chars: &mut VecDeque<char>, expected: char) {
+        match chars.pop_front() {
+            None => panic!("Character '{}' expected, found nothing", expected),
+            Some(c) if c == expected => (),
+            Some(c) => panic!("Character '{}' expected, found '{}'", expected, c),
+        }
+    }
+
+    fn parse_number(chars: &mut VecDeque<char>) -> i64 {
+        let s = chars.iter().collect::<String>();
+        s.parse::<i64>().expect("Number expected")
+    }
+}
+
+fn resolve_monkey(monkeys: &HashMap<String, Monkey>, name: &str) -> Option<i64> {
+    let Some(m) = monkeys.get(name) else { return None };
+
+    match &m.yell {
+        Yell::Num(n) => Some(*n),
+        Yell::Op(OpType::Add, op1, op2) => {
+            Some(resolve_monkey(monkeys, op1)? + resolve_monkey(monkeys, op2)?)
+        }
+        Yell::Op(OpType::Sub, op1, op2) => {
+            Some(resolve_monkey(monkeys, op1)? - resolve_monkey(monkeys, op2)?)
+        }
+        Yell::Op(OpType::Mul, op1, op2) => {
+            Some(resolve_monkey(monkeys, op1)? * resolve_monkey(monkeys, op2)?)
+        }
+        Yell::Op(OpType::Div, op1, op2) => {
+            Some(resolve_monkey(monkeys, op1)? / resolve_monkey(monkeys, op2)?)
+        }
+    }
+}
+
+fn resolve_monkey_inverse(monkeys: &HashMap<String, Monkey>, name: &str, target: i64) -> i64 {
+    if name == "humn" {
+        return target;
+    }
+
+    let node = monkeys.get(name).expect("node not found");
+    let Yell::Op(op_type, branch1, branch2) = &node.yell else { panic!("name is not an operation") };
+
+    let res1 = resolve_monkey(monkeys, branch1);
+    let res2 = resolve_monkey(monkeys, branch2);
+
+    let (human_branch, op_value, human_left_side) = match (res1, res2) {
+        (None, None) => panic!("Human on both sides"),
+        (Some(_), Some(_)) => panic!("Human on neither side"),
+        (None, Some(v)) => (branch1, v, true),
+        (Some(v), None) => (branch2, v, false),
+    };
+
+    let human_branch_target = match (op_type, human_left_side) {
+        (OpType::Add, _) => target - op_value,
+        (OpType::Sub, true) => target + op_value,
+        (OpType::Sub, false) => op_value - target,
+        (OpType::Mul, _) => target / op_value,
+        (OpType::Div, true) => target * op_value,
+        (OpType::Div, false) => op_value / target,
+    };
+
+    resolve_monkey_inverse(monkeys, human_branch, human_branch_target)
+}
+
+pub fn test1() {
+    let mut monkeys = HashMap::new();
+
+    for m in utils::parse_lines("./data/day21.txt", Monkey::parse).drain(..) {
+        monkeys.insert(m.name.clone(), m);
+    }
+
+    let root = resolve_monkey(&monkeys, "root");
+    println!("Root: {}", root.unwrap());
+}
+
+pub fn test2() {
+    let mut monkeys = HashMap::new();
+
+    for m in utils::parse_lines("./data/day21.txt", Monkey::parse).drain(..) {
+        if m.name != "humn" {
+            monkeys.insert(m.name.clone(), m);
+        }
+    }
+
+    let root = monkeys.get_mut("root").expect("root not found");
+    let Yell::Op(_, branch1, branch2) = &root.yell else { panic!("root is not an operation") };
+    root.yell = Yell::Op(OpType::Sub, branch1.clone(), branch2.clone());
+
+    let human_value = resolve_monkey_inverse(&monkeys, "root", 0);
+    println!("HUMN: {}", human_value);
+}
